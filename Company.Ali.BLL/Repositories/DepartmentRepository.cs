125b641e549f886f6e0a54234e02f6b91d64aeb8
ali <altir1781@gmail.com>
2025-03-08 17:25:20 +0200

    Department Module BLL

---
+++ Company.Ali.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,65 @@
+﻿using Company.Ali.BLL.Interfaces;
+using Company.Ali.DAL.Data.Contexts;
+using Company.Ali.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Ali.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context; // NULL
+
+        public DepartmentRepository()
+        {
+            _context = new CompanyDbContext();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+           
+
+            return _context.Departments.ToList();
+        }
+
+        public Department? Get(int id)
+        {
+          
+
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+
+
+            _context.Departments.Add(model);
+
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department model)
+        {
+        
+
+            _context.Departments.Update(model);
+
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+
+
+            _context.Departments.Remove(model);
+
+            return _context.SaveChanges();
+        }
+
+
+    
+    }
+}
